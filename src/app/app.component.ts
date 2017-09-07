0dfff3df69e092375aece3378a4b61b5ca5d2f5d
Mathew <mathewthomas358@gmail.com>
2017-09-07 23:56:49 +0530

    Centered convolution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <div>	
-  <h1>{{title}}</h1>
-  </div>
-  `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Convolution';
